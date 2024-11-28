1ded5aa1d3eb270ab9192bcb47e644022dec7347
Anna Vasileva <ann.wider99@gmail.com>
2024-11-28 10:48:54 +0300

    testing index.js

--- toDoListApp/index.js
+++ toDoListApp/index.js
@@ -1,1 +1,14 @@
+let form = document.getElementById("form");
+let taskInput = document.getElementById("taskInput");
+let submitButton = document.getElementById("submitButton");
+let tasksSection = document.getElementById("tasksSection");
+let tasks = document.getElementById("tasks");
 
+submitButton.onclick = function() {
+  console.log("test");
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newTaskInputValue = form.elements.taskInput;
+})
