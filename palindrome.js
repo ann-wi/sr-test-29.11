dc45aa1d3b9d3577bcb71f4fa429540522098491
Anna Vasileva <ann.wider99@gmail.com>
2024-11-28 16:34:05 +0300

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,11 @@
 function isPalindrome(str) {
-    //
+    let reverse = str.split("").reverse().join("");
+
+    if (rev == str) {
+        return true;
+    }
+
+    return false;
 }
+
+console.log(isPalindrome("А роза упала на лапу Азора"));
