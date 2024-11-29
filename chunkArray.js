0590b89c272bc682395b2f0e58fd467a7a6add18
Anna Vasileva <ann.wider99@gmail.com>
2024-11-29 15:00:43 +0300

    Update chunkArray.js

--- chunkArray.js
+++ chunkArray.js
@@ -1,1 +1,8 @@
-
+function chunkArray(arr, size) {
+   if (size <= 0) return;
+  
+   return arr.reduce((acc, _, i) => {
+   if (i % size === 0) acc.push(arr.slice(i, i + size));
+   return acc;
+   }, []);
+}
